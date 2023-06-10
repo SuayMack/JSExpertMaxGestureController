3bf641c96b716b3842b8aae70488450085726fe6
SuayMack <pry_chu@hotmail.com>
2023-06-09 21:23:03 -0300

    first commit

---
+++ pages/lib/shared/camera.js
@@ -0,0 +1,39 @@
+export default class Camera {
+  constructor() {
+    this.video = document.createElement('video')
+  }
+  static async init() {
+    if(!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+      throw new Error(
+        `Browser API navigator.mediaDevices.getUserMedia not avaiable`
+      )
+    }
+    const videoConfig = {
+      audio: false,
+      video: {
+        width: globalThis.screen.availWidth,
+        height: globalThis.screen.availHeight,
+        frameRate: {
+          ideal: 60
+        }
+      }
+    }
+    const stream = await navigator.mediaDevices.getUserMedia(videoConfig)
+    const camera  =  new Camera()
+    camera.video.srcObject = stream
+
+    //debug reasons!
+    camera.video.height = 240
+    camera.video.width = 240
+    document.body.append(camera.video)
+    
+    //aguarda pela camera
+    await new Promise((resolve) => {
+      camera.video.onloadedmetadata = () => {
+        resolve(camera.video)
+      }
+    })
+    camera.video.play()
+    return camera
+  }
+}
---
+++ pages/video-player/src/worker.js
@@ -0,0 +1,27 @@
+import "https://unpkg.com/@tensorflow/tfjs-core@2.4.0/dist/tf-core.js"
+//import "https://unpkg.com/@tensorflow/tfjs-backend-webgl@2.4.0/dist/tf-backend-webgl.js"
+import "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs-backend-cpu@2.4.0/dist/tf-backend-cpu.js"
+
+import "https://unpkg.com/@tensorflow/tfjs-converter@2.4.0/dist/tf-converter.js"
+import "https://unpkg.com/@tensorflow-models/face-landmarks-detection@0.0.1/dist/face-landmarks-detection.js"
+
+import Service  from "./service.js"
+//no processo principal é window
+//no worker é self
+
+const { tf, faceLandmarksDetection } = self
+tf.setBackend('cpu')
+
+const service = new Service({
+  faceLandmarksDetection
+})
+console.log('loading tf model')
+await service.loadModel()
+console.log('tf model loaded!')
+postMessage('READY')
+
+onmessage = async ({ data: video }) => {
+  const blinked = await service.handBlinked(video)
+  if(!blinked) return;
+  postMessage({ blinked })
+}
