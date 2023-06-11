6a4e3f4eef425c8f0de6c0fb92d1654a55f85fd8
SuayMack <pry_chu@hotmail.com>
2023-06-11 20:23:10 -0300

    update bugs

--- pages/titles/src/factories/cardsFactory.js
+++ pages/titles/src/factories/cardsFactory.js
@@ -1,10 +1,19 @@
 import CardsController from "../controllers/cardsController.js"
 import CardsView from "../views/cardsView.js"
 import CardsService from "../services/cardsService.js"
+
+
+const cardListWorker = new Worker(`./src/workers/cardListWorker.js`, {type: "module"})
+cardListWorker.onmessage = (msg) => {
+  console.log('processo principal', msg)
+}
+
+cardListWorker.postMessage('Hey Hello!!!')
 const [rootPath] = window.location.href.split('/pages/')
 const factory = {
   async initialize() {
     return CardsController.initialize({
+      worker: cardListWorker,
       view: new CardsView(),
       service: new CardsService({ dbUrl: `${rootPath}/assets/database.json` })
     })
